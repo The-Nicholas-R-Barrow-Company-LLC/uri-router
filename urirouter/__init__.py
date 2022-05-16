f174f7d0a81e64545ba7604d767281567654000a
barrownicholas <me@nicholasrbarrow.com>
2022-05-16 19:48:29 -0400

    1.0.0

--- urirouter/__init__.py
+++ urirouter/__init__.py
@@ -1,8 +1,30 @@
 import functools
 from urllib.parse import urlparse
+import logging
 
 
-# TODO: eliminate all print statements with better logging
+_logger = logging.Logger(__name__)
+streamFormatter = logging.Formatter("[URIRouter] %(levelname)s: %(message)s")
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+streamHandler.setFormatter(streamFormatter)
+_logger.addHandler(streamHandler)
+
+
+class RoutingError(Exception):
+    """Exception raised for errors in routes and routing.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message=None):
+        self.message = message
+        if message:
+            _logger.error(self.message)
+            super().__init__(self.message)
+        else:
+            super().__init__()
 
 
 class _Route:
@@ -12,11 +34,7 @@ class _Route:
     def __new__(cls, route, func, *args, **kwargs):
         for instance in cls.__instances__:
             if instance.comparator == cls.get_comparator(route):
-                raise RuntimeError("route already exists!")
-                # print("returning existing instance")
-                # return instance
-        print("creating new instance")
-        print(args, kwargs)
+                raise RoutingError(f"attempted to register existent route ({route})")
         instance = super(_Route, cls).__new__(cls)
         _Route.__instances__.append(instance)
         return instance
@@ -36,8 +54,10 @@ class _Route:
         self.func = func
         self.args = args
         self.kwargs = kwargs
-        # TODO: remove print statement w/ better logging
-        print(f"new route created! {route=}, {func=}, {args=}, {kwargs=}")
+        _logger.info(f"created {self}")
+
+    def __repr__(self):
+        return f"_Route(route={self.route}, func={self.func}, args={self.args}, kwargs={self.kwargs})"
 
     @property
     def comparator(self) -> str:
@@ -56,47 +76,57 @@ class URIRouter:
     def __new__(cls, scheme, *args, **kwargs):
         for instance in cls.__instances__:
             if instance.scheme == scheme:
-                print("returning existing instance")
+                _logger.warning(f"attempting to create a new URIRouter with existing scheme; URIRouter's must have unique schemes, returning existing scheme (returning the existing URIRouter instance)")
                 return instance
-        print("creating new instance")
+        _logger.info(f"creating new URIRouter")
         instance = super(URIRouter, cls).__new__(cls, *args, **kwargs)
         URIRouter.__instances__.append(instance)
         return instance
 
     def __init__(self, scheme, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.scheme = scheme
-        self.__routes__: "list[_Route]" = []
+        if not hasattr(self, 'scheme'):
+            self.scheme = scheme
+        if not hasattr(self, '__routes__'):
+            self.__routes__: "list[_Route]" = []
+
+    def __repr__(self):
+        return f"URIRouter('{self.scheme}://...')"
 
     def route(self, route, *args, **kwargs):
-        print("handling route")
+        _logger.debug(f"registering route {route}")
         parsed = urlparse(route)
         built_uri = f"{self.scheme}://{parsed.netloc}{parsed.path}"
+        if parsed.scheme != self.scheme:
+            raise RoutingError(f"{self} cannot handle a route with scheme '{parsed.scheme}' (self.scheme != route's scheme)")
         def decorator(func):
-            print("in func")
-            # TODO: only create route (1) if scheme matches and (2) if not already exists
             self.__routes__.append(_Route(built_uri, func, *args, **kwargs))
+            _logger.debug(f"registered route {route}")
             @functools.wraps(func)
             def inner_function_handling_gateway(*args, **kwargs):
-                # self.__routes__.append(_Route(uri, func))
                 pass
             return inner_function_handling_gateway
         return decorator
 
-    def handle(self, uri):
-        print("attempting to handle")
+    def handle(self, uri) -> bool:
+        """
+        Handle an incoming uri and its parameters.
+        :param uri: the incoming uri.
+        :return: True, if a registered route is found; False, if a registered route is not found
+        """
+        _logger.info(f"{self} received uri: {uri}")
         parsed = urlparse(uri)
         for route in self.__routes__:
-            print(route.comparator, "=?=", _Route.get_comparator(uri))
+            _logger.debug(f"{route.comparator} =?= {_Route.get_comparator(uri)}")
             if route.comparator == _Route.get_comparator(uri):
-                print("found!")
+                _logger.info(f"{self} found a route for {uri}")
                 for item in parsed.query.split("&"):
-                    print(item)
+                    _logger.debug(f"got kwarg in uri: {item}")
                     key, value = item.split("=")
                     if key in route.kwargs.keys():
-                        # TODO: better logging and handling
-                        print("Key Exists! Overriding")
+                        _logger.warning(f"'{key}' already exists in kwargs; overriding its value with '{value}'")
                     route.kwargs[key] = value
                 route.func(*route.args, **route.kwargs)
                 return True
+        _logger.warning(f"{self} failed to find a route for {uri}")
         return False
