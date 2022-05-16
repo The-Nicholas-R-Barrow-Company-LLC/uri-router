c9ae2e64ad5ad873c94f13ea4cfbde01c1fe59fd
barrownicholas <me@nicholasrbarrow.com>
2022-05-16 18:56:15 -0400

    commit

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="Flask-style routing for URIs",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp",
+    url="https://github.com/The-Nicholas-R-Barrow-Company-LLC/uri-router",
     author="The Nicholas R. Barrow Company, LLC",
     author_email="me@nicholasrbarrow.com",
     license="MIT",
@@ -30,8 +30,8 @@ setup(
     include_package_data=True,
     install_requires=[],
     project_urls={
-        'Source': 'https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp',
-        'Tracker': 'https://github.com/The-Nicholas-R-Barrow-Company-LLC/PyMacApp/issues',
+        'Source': 'https://github.com/The-Nicholas-R-Barrow-Company-LLC/uri-router',
+        'Tracker': 'https://github.com/The-Nicholas-R-Barrow-Company-LLC/uri-router/issues',
     }
         # 'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
 )
